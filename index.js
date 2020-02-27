38a0af6a385b100992b8c137124066da06ff839a
Tracy Collins <tc@threeceemedia.com>
2020-02-27 06:55:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(line) {
   for (var i=0; i<line.length; i++){
     text = text + `${i+1}. ${line[i]}`
     if (i!==line.length-1){
-      text = text + ","
+      text = text + ", "
     }
   }
   return text;
